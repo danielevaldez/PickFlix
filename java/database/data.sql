dc453b0f4886fb3c80ebfcaf423d9c7f436e50f8
Michael OHare <mjohare13@gmail.com>
2023-08-08 09:34:34 -0400

    profile, genre, and profile_genre table creation

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,10 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO genre (genre_name) VALUES ('Comedy');
+INSERT INTO genre (genre_name) VALUES ('Horror');
+INSERT INTO genre (genre_name) VALUES ('Action');
+INSERT INTO genre (genre_name) VALUES ('Sci-Fi');
+INSERT INTO genre (genre_name) VALUES ('Fantasy');
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,33 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, profile, genre, profile_genre;
 
 CREATE TABLE users (
-	user_id SERIAL,
+	user_id SERIAL NOT NULL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE profile (
+	profile_id SERIAL PRIMARY KEY,
+	user_id int NOT NULL,
+	profile_name varchar(50) NOT NULL,
+	CONSTRAINT FK_profile_users FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE genre (
+	genre_id SERIAL PRIMARY KEY,
+	genre_name varchar(50) NOT NULL
+);
+
+CREATE TABLE profile_genre (
+	profile_id int NOT NULL,
+	genre_id int NOT NULL,
+	PRIMARY KEY (profile_id, genre_id),
+	CONSTRAINT FK_pg_profile FOREIGN KEY (profile_id) REFERENCES profile (profile_id),
+	CONSTRAINT FK_pg_genre FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
 );
 
 COMMIT TRANSACTION;
