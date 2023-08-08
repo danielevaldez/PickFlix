3839afa9ab1be25adb146e7ac82a2b846ce009cc
Michael OHare <mjohare13@gmail.com>
2023-08-08 17:10:18 -0400

    Project structure outline

---
+++ java/src/main/java/com/techelevator/dao/GenreDao.java
@@ -0,0 +1,11 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Genre;
+
+import java.util.List;
+
+public interface GenreDao {
+    List<Genre> getGenres();
+    Genre getGenreById(int id);
+    Genre getGenreByName(String name);
+}
---
+++ java/src/main/java/com/techelevator/dao/ProfileDao.java
@@ -0,0 +1,11 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Profile;
+
+import java.util.List;
+
+public interface ProfileDao {
+    List<Profile> getProfiles();
+    Profile getProfileById(int id);
+    List<Profile> getProfilesByUserId(int id);
+}
